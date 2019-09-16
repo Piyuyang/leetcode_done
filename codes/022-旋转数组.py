dc6085ab768675936d296546b418278a48018b1a
Piyuyang <pi_yuyang@163.com>
2019-09-16 21:39:03 +0800

    旋转数组-思路一

---
+++ codes/022-旋转数组.py
@@ -0,0 +1,29 @@
+"""
+给定一个数组，将数组中的元素向右移动 k 个位置，其中 k 是非负数
+
+输入: [1,2,3,4,5,6,7] 和 k = 3
+输出: [5,6,7,1,2,3,4]
+解释:
+向右旋转 1 步: [7,1,2,3,4,5,6]
+向右旋转 2 步: [6,7,1,2,3,4,5]
+向右旋转 3 步: [5,6,7,1,2,3,4]
+"""
+
+
+class Solution(object):
+    def rotate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        for i in range(k):
+            tmp = nums.pop()
+            nums.insert(0, tmp)
+
+
+if __name__ == '__main__':
+    nums = [1, 2, 3, 4, 5, 6, 7]
+    s = Solution()
+    s.rotate(nums, 3)
+    print(nums)
